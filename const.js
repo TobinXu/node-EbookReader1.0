77c4f554c6a11aafc89dc5c975d6a4d473bae1b8
TobinXu <tommybug@163.com>
2020-10-24 14:41:30 +0800

    提交后端源码

---
+++ const.js
@@ -0,0 +1,32 @@
+const resUrl = 'http://127.0.0.1:9001'
+const mp3FilePath = 'E:\\kresource\\mp3'
+
+const category = [
+    'Biomedicine',
+    'BusinessandManagement',
+    'ComputerScience',
+    'EarthSciences',
+    'Economics',
+    'Engineering',
+    'Education',
+    'Environment',
+    'Geography',
+    'History',
+    'Laws',
+    'LifeSciences',
+    'Literature',
+    'SocialSciences',
+    'MaterialsScience',
+    'Mathematics',
+    'MedicineAndPublicHealth',
+    'Philosophy',
+    'Physics',
+    'PoliticalScienceAndInternationalRelations',
+    'Psychology',
+    'Statistics'
+]
+module.exports = {
+    resUrl,
+    category,
+    mp3FilePath
+}
